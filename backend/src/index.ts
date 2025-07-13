f9bb4c36f77ed2398a96f75cc9af799ec450bd59
Stella-055 <stellaongayo243@gmail.com>
2025-07-14 01:35:52 +0300

    changed baseurl

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(cookie());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blogingit.netlify.app",
     credentials: true,
   }),
 );
