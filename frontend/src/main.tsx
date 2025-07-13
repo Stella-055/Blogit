c69da2e5a1bc25493e55f97097039e0a9646e300
Stella-055 <stellaongayo243@gmail.com>
2025-07-14 00:43:14 +0300

    fixed build errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "./components/ui/sonner.tsx";
+
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={client}>
       <BrowserRouter>
         <App />
-        <Toaster />
+     
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
