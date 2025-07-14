c073b9a3a855218adf7713ba6ecb38562a2355c9
Stella-055 <stellaongayo243@gmail.com>
2025-07-14 17:00:04 +0300

    removed h-screen on hero

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <>
       <div className=" flex  flex-col justify-center items-center  ">
-        <div className="flex items-center gap-4 justify-center flex-wrap w-full">
+        <div className="flex items-center gap-4 justify-center flex-wrap w-full mt-24">
           <Profilenav />
           <Profiledetails />
         </div>
