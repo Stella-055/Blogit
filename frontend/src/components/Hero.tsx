17bc58a4dbac3f260af62c124620f1b5b5ca21ad
Stella-055 <stellaongayo243@gmail.com>
2025-07-14 16:24:12 +0300

    added margin

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -6,9 +6,9 @@ const Hero = () => {
     <div>
       <section
         style={{ height: "100vh" }}
-        className="bg-white lg:grid lg:h-screen lg:place-content-center  flex items-center mt-20"
+        className="bg-white lg:grid lg:h-screen lg:place-content-center  flex items-center "
       >
-        <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+        <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32 pt-20">
           <div className="mx-auto max-w-prose text-center">
             <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
               Come Manage Your Blogs
