ca106bbf9f491de49bba6bd0e22e77c5367b3dd9
Stella-055 <stellaongayo243@gmail.com>
2025-07-14 19:16:50 +0300

    implemented otp sending capabilities

--- frontend/src/pages/Otp.tsx
+++ frontend/src/pages/Otp.tsx
@@ -4,19 +4,58 @@ import {
   InputOTPSlot,
 } from "@/components/ui/input-otp";
 import { Button } from "@mui/material";
-
+import { useMutation } from "@tanstack/react-query";
+import api from "@/Api/Axios";
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {Alert} from "@mui/material";
+import { useParams } from "react-router-dom";
+   
 const Otp = () => {
-  const [value, setValue] = useState("");
+  const navigate=useNavigate()
+  type userotp={
+    otp:string
+  }
+  const {id}=useParams()
+  const [value, setValue] = useState <userotp>({otp:""});
+  const [errors, setErrors] = useState<null | string>();
+  const { isPending, mutate } = useMutation({
+    mutationKey: ["OTP"],
+    mutationFn: async (otp:userotp) => {
+      const result = await api.post(`/api/user/forgotpassword/${id}`, otp);
+      return result.data;
+    },
+
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        setErrors(error.response?.data.message);
+        return;
+      } else {
+        setErrors("something went wrong");
+        return;
+      }
+    },
+    onSuccess: () => {
+     
+      navigate("/")
+     
+    },
+  });
+
+  function submitotp(){
+    mutate(value)
+  }
   return (
     <div className="flex justify-center items-center h-screen ">
       <div className="border p-14">
         <div className="space-y-2">
+              {errors && <Alert severity="error">{errors}</Alert>}
           <h2 className="text-lg">One-Time Password</h2>
           <InputOTP
             maxLength={6}
-            value={value}
-            onChange={(value) => setValue(value)}
+            value={value.otp}
+            onChange={(otp) => setValue({ otp })}
           >
             <InputOTPGroup>
               <InputOTPSlot index={0} />
@@ -31,7 +70,7 @@ const Otp = () => {
             {" "}
             Please enter the one-time password sent to your phone.{" "}
           </h2>
-          <Button variant="contained"> Submit</Button>
+          <Button onClick={submitotp} loading={isPending} variant="contained"> Submit</Button>
         </div>
       </div>
     </div>
